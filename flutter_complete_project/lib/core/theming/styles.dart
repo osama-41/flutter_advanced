eca90a9489f350d0cc9fec9aee6383159f5864bf
osama-41 <osama.tarefee123@gmail.com>
2025-10-20 17:00:26 +0300

    onboarding changes
    - Added images & SVGs folders for assets
    - Added flutter_svgs & added flutter_screenutil
    - Made a file for doc app client
    - Added Navigation extention
    - Added routing folder that contains router class & routes file
    - Added colors & textStyles
    - Added empty login screen
    - Added onboardin screen

---
+++ flutter_complete_project/lib/core/theming/styles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_project/core/theming/colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TextStyles {
+  static TextStyle font24Black700Weight = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static TextStyle font32BlueBold = TextStyle(
+    fontSize: 32.sp,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.mainBlue,
+  );
+  static TextStyle font13GreyRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.normal,
+    color: ColorsManager.grey,
+  );
+  static TextStyle font16WhiteSimiBold = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+}
